f5237ef8e4841102bbc2929d9e92d40d3c027cdf
marlon castro <macastroc@sysdatec.edu.co>
2025-07-17 20:15:30 -0500

    Creacion Proyecto Guia

---
+++ public/ver-artesano.php
@@ -0,0 +1,80 @@
+<html>
+    <head lang="es">
+        <link rel="stylesheet" href="styles\amparo1.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>artesanos</title>
+    </head>
+    <body>
+        <header>
+        </header>
+        <nav>
+        </nav>
+        <main><center>
+            <div class="cuadros">
+                <div class="cuadroizquierda1">
+                    <div class="cuadroizquierda2"><center>
+                        <img src="" width="320px" height="275" alt="no hay yippee imagen XD"> </center>
+                        <div class="cuadroizquierda3">
+                            <h1>Amparo Rodriguez Ruiz</h3>
+                        </div>
+                        <div class="cuadroizquierda4">
+                            <h2>Historia</h4>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+                    <div class="cuadroderecha1">
+                        <h3>Productos del Artesano</h3>
+                        <div class="cuadroderecha2">
+                            <div class="cuadroderecha3">
+                                <center>
+                                    <img src="Portatelefonos.jpeg"width="213px"height="180px"alt="no hay yippee imagen XD">
+                                    <p>soportes para telefonos</p>
+                                    <button type="submit">Ver más</button>
+                                </center>
+                            </div>
+                            <div class="cuadroderecha3">
+                                <center>
+                                    <img src="Manualidades.jpeg"width="213px"height="180px"alt="no hay yippee imagen XD">
+                                    <p>Manualidades</p>
+                                    <button type="submit">Ver más</button>
+                                </center>
+                            </div>
+                            <div class="cuadroderecha3">
+                                <center>
+                                    <img src="C:/xampp/htdocs/artesanos/images/Gargantillas.jpeg"width="213px"height="180px"alt="no hay yippee imagen XD">
+                                    <p>Gargantillas</p>
+                                    <button type="submit">Ver más</button>
+                                </center>
+                            </div>
+                            <br>
+                            <br>
+                            <br>
+                            <div class="cuadroderecha3">
+                                <center>
+                                    <img src="/images/Macetas.jpeg"width="213px"height="180px"alt="no hay yippee imagen XD">
+                                    <p>Macetas en ceramica</p>
+                                    <button type="submit">Ver más</button>
+                                </center>
+                            </div>
+                            <div class="cuadroderecha3">
+                                <center>
+                                    <img src="Metal.jpeg"width="213px"height="180px"alt="no hay yippee imagen XD">
+                                    <p>Soporte de metal y macetas plasticas</p>
+                                    <button type="submit">Ver más</button>
+                                </center>
+                            </div>
+                            <div class="cuadroderecha3">
+                                <center>
+                                    <img src="Mecedoras.jpeg"width="213px"height="180px"alt="no hay yippee imagen XD">
+                                    <p>Mecedoras</p>
+                                    <button type="submit">Ver más</button>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </center>
+        </main>
